da368507dc6760271cefebac8719c77f5586362f
double J, T <bestjun111@gmail.com>
2021-08-30 07:23:38 +0900

    maddeee

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,15 @@
 function showPopup(n){
     const elem = document.querySelectorAll('.popup')[n-1];
     elem.classList.add('visible');
-    const background = document.getElementById('popupHider');
+    const background = document.querySelector('#popupHider');
     background.classList.add('visible');
-    console.log('popup');
 }
 
 function hidePopup(){
-    const elem = document.querySelector('.visiblePopup');
+    const elem = document.querySelector('.visible.popup');
     elem.classList.remove('visible');
-    const background = document.getElementById('popupHider');
+    elem.classList.add('invisible');
+    const background = document.querySelector('#popupHider');
     background.classList.remove('visible');
-    console.log('popup-');
+    background.classList.add('invisible');
 }
