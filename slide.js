ce9232ef90ea95faf30d11a5692f0238151cc441
Juntae Jeong <juntaejeonghappy@naver.com>
2021-10-10 18:39:05 +0900

    일단 Commit...

--- slide.js
+++ slide.js
@@ -1,7 +1,16 @@
-
+let interval;
 let slideIndex=0;
 const slides= document.getElementsByClassName("slide");
 const slideCount = slides.length;
+
+function keydown(e){
+    if(e.code=='ArrowLeft'){
+        slideprev();
+    }
+    if(e.code=='ArrowRight'){
+        slidenext();
+    }
+}
 
 function slidenext(){
     slides[slideIndex].classList.remove('show');
@@ -14,11 +23,13 @@ function slidenext(){
         }
     }
     slides[slideIndex].classList.add('show');
+
+    resetTimer();
 }
 
 
 function slideprev(){
-    slides[slideIndex].classList.remove('show');    
+    slides[slideIndex].classList.remove('show');
 
     slideIndex--;
     if(slideIndex<0) {
@@ -29,10 +40,15 @@ function slideprev(){
     }
     slides[slideIndex].classList.add('show');
     slides[slideIndex].classList.remove('hide');
+
+    resetTimer();
 }
 
 
+function resetTimer(){
+    clearInterval(interval);
+    interval=setInterval(slidenext,10000);
+}
 
 
-
-setInterval(slidenext,10000);
+interval=setInterval(slidenext,10000);
